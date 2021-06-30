162c78fab5e46cfc149b7fff57616c6290a1e132
Cornel Zgardan <zgardancornel@gmail.com>
2021-07-01 00:57:42 +0300

    visit entity

--- pet-clinic-data/src/main/java/ro/iss/guruclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ro/iss/guruclinic/model/Owner.java
@@ -1,6 +1,25 @@
 package ro.iss.guruclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
   private static final long serialVersionUID = -5516376692037035026L;
+
+  private Set<Pet> pets;
+
+  /** @return the pets */
+  public Set<Pet> getPets() {
+    return pets;
+  }
+
+  /** @param pets the pets to set */
+  public void setPets(Set<Pet> pets) {
+    this.pets = pets;
+  }
+
+  /** @return the serialversionuid */
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
 }
