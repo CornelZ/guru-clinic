167be0e651d9996ae105425c5d5e32b9d2b589d9
Cornel Zgardan <zgardancornel@gmail.com>
2021-07-01 02:53:37 +0300

    broken links

--- pet-clinic-data/src/main/java/ro/iss/guruclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/ro/iss/guruclinic/services/map/VetServiceMap.java
@@ -3,13 +3,29 @@ package ro.iss.guruclinic.services.map;
 import java.util.Set;
 import org.springframework.stereotype.Service;
 import ro.iss.guruclinic.model.Vet;
+import ro.iss.guruclinic.services.SpecialtyService;
 import ro.iss.guruclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
+  private final SpecialtyService specialtyService;
+
+  public VetServiceMap(SpecialtyService specialtyService) {
+    this.specialtyService = specialtyService;
+  }
+
   @Override
   public Vet save(Vet obj) {
+    if (obj.getSpecialties().size() >= 0) {
+      obj.getSpecialties()
+          .forEach(
+              spec -> {
+                if (spec.getId() == null) {
+                  specialtyService.save(spec);
+                }
+              });
+    }
     return super.save(obj);
   }
 
