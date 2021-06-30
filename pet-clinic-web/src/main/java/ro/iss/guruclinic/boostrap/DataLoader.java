de869953d63a1497c0f65f15215475d7da8e1f06
Cornel Zgardan <zgardancornel@gmail.com>
2021-07-01 01:15:39 +0300

    pettype bootstrap

--- pet-clinic-web/src/main/java/ro/iss/guruclinic/boostrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ro/iss/guruclinic/boostrap/DataLoader.java
@@ -3,8 +3,10 @@ package ro.iss.guruclinic.boostrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ro.iss.guruclinic.model.Owner;
+import ro.iss.guruclinic.model.PetType;
 import ro.iss.guruclinic.model.Vet;
 import ro.iss.guruclinic.services.OwnerService;
+import ro.iss.guruclinic.services.PetTypeService;
 import ro.iss.guruclinic.services.VetService;
 
 @Component
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(
+      OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) throws Exception {
+
+    PetType dog = new PetType();
+    dog.setName("Dog");
+    petTypeService.save(dog);
+
+    PetType cat = new PetType();
+    cat.setName("cat");
+    petTypeService.save(cat);
 
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
