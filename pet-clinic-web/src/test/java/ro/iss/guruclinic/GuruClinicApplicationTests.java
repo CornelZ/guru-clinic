5c161192eb7b932d2177daebcc81b804b01b8797
Cornel Zgardan <zgardancornel@gmail.com>
2021-07-19 21:37:48 +0300

    convert to junit5

--- pet-clinic-web/src/test/java/ro/iss/guruclinic/GuruClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/ro/iss/guruclinic/GuruClinicApplicationTests.java
@@ -1,13 +1,14 @@
 package ro.iss.guruclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class GuruClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
